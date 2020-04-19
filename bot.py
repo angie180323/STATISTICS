d97482ca90dce70ef76f76382b4ef06ba70a96b1
Angie Rodriguez <angie@Angies-MacBook-Pro.local>
2020-04-18 23:08:35 -0400

    Median

--- bot.py
+++ bot.py
@@ -17,6 +17,16 @@ async def on_ready():
 async def say_hello(ctx):
     await ctx.send(f"Hi {ctx.author.display_name}")
 
+@bot.command(name="median", help="gives you median")
+async def median(A, ctx):
+    m = len(A)
+    A.sort()
+    if m % 2 == 0:
+        med1 = A[m//2]
+        med2 = A[m//2 - 1]
+        med = (med1 + med2)/2
+    else:
+        med = A[m//2]
+    await ctx.send(med)
+
 bot.run(TOKEN)
-
-
